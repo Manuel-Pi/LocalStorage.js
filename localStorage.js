994942b94003ef1e54fc7c07fb57410ee913fcff
Manuel_Pi <psykofou@hotmail.com>
2015-07-22 23:16:34 +0200

    Use the Universal Module Definition

--- localStorage.js
+++ localStorage.js
@@ -1,26 +1,28 @@
-define([],
-function(){
-	
-});
-
-// if the module has no dependencies, the above pattern can be simplified to
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define([], factory);
-    } else if (typeof exports === 'object') {
+    } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
         module.exports = factory();
     } else {
         // Browser globals (root is window)
-        root.returnExports = factory();
+        root.IDB = factory();
   }
 }(this, function () {
-
-    // Just return a value to define the module export.
-    // This example returns an object, but the module
-    // can return a function as the exported value.
-    return {};
+    if(!localStorage){
+		console.log('Local storage not avaiable!');
+		return;
+	} else{
+		return {
+			save : function(key, value){
+				localStorage.setItem(key, JSON.stringify(value));
+			},
+			get : function(key){
+				return JSON.parse(localStorage.getItem(key));
+			}
+		};
+	}
 }));
